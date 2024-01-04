92e6c3a008a08d74d67f7b666bf78929816e9349
yhj <yhj@yhjui-MacBookPro.local>
2024-01-04 16:26:47 +0900

    third commit shap install

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,8 @@ import { NextResponse } from "next/server";
 export async function middleware() {
   const session = await auth();
   if (!session) {
-    return NextResponse.redirect("http://localhost:3000/i/flow/login");
+    return NextResponse.redirect("http://localhost:3000/i/flow/login"); //프론트의 포트번호를 바꾼다.
+    // return NextResponse.redirect("http://localhost:80/i/flow/login");   //프론트의 포트번호를 바꾼다.
   }
 }
 
